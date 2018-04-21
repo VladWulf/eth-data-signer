e5080cc6ef05d9a3bb0b5fd8fedace194adaf847
VladWulf <agaev.vladimir@gmail.com>
2018-04-21 23:44:42 +0100

    cleaned test

--- test/sigTest.js
+++ test/sigTest.js
@@ -4,30 +4,13 @@ const sign = require('../index');
 describe('test for ecverify', () => {
   it('should return an object', () => {
     const res = sign('hello', 'cea952fcb6463ed79eadbc9378050746784d71ba67df33f0d2dd3f234244ff51')
-    console.log(res)
     expect(res).to.be.an('object');
   })
   it('should return an correct arguments', () => {
     const res = sign('hello', 'cea952fcb6463ed79eadbc9378050746784d71ba67df33f0d2dd3f234244ff51')
-    expect(res.originalMsg).to.equal('hello');
-    expect(res.hashedMsg).to.equal('0xcd87f010dd87d1d0b609e378c9b368623e1789d83d10a64721b4751948e8381d');
-    expect(res.signature).to.equal('0x4d1f55c921a3cfc5538f83f02741f81fa846a9d555c4d193887163a8ec1a79654041a61368febe6cf814b6abc49f842f8ce059c18171dfd6e258f760a4ee05b2');
-  })
-})
-describe('test for ecrecover', () => {
-  it('should return an object', () => {
-    const res = sign('hello', 'cea952fcb6463ed79eadbc9378050746784d71ba67df33f0d2dd3f234244ff51', true)
-    console.log(res)
-    expect(res).to.be.an('object');
-    expect(res.signature).to.be.an('object');
-  })
-  it('should return an correct arguments', () => {
-    const res = sign('hello', 'cea952fcb6463ed79eadbc9378050746784d71ba67df33f0d2dd3f234244ff51', true)
-    expect(res.originalMsg).to.equal('hello');
-    expect(res.hashedMsg).to.equal('0xcd87f010dd87d1d0b609e378c9b368623e1789d83d10a64721b4751948e8381d');
-    expect(res.signature.v).to.equal(27);
-    expect(res.signature.r).to.equal('0x4d1f55c921a3cfc5538f83f02741f81fa846a9d555c4d193887163a8ec1a7965');
-    expect(res.signature.s).to.equal('0x4041a61368febe6cf814b6abc49f842f8ce059c18171dfd6e258f760a4ee05b2');
+    expect(res.originalData).to.equal('hello');
+    expect(res.hashedData).to.equal('0xcd87f010dd87d1d0b609e378c9b368623e1789d83d10a64721b4751948e8381d');
+    expect(res.signature).to.equal('0x4d1f55c921a3cfc5538f83f02741f81fa846a9d555c4d193887163a8ec1a79654041a61368febe6cf814b6abc49f842f8ce059c18171dfd6e258f760a4ee05b21b');
   })
 })
 
